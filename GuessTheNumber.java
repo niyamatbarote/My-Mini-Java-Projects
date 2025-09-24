18ef2fb7a04364c1bcab70b7f403e024193fe689
niyamat <niyamatbarote@gmail.com>
2025-09-24 22:08:31 +0530

    Added Limit And Multiple Attempts

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -2,52 +2,34 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class GuessTheNumber {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rd = new Random();
+        int random = rd.nextInt(201);
 
-        // While (True) for Continuing Game (Try Again) OPTION Type
-        while (true) {
-            int random = rd.nextInt(201);
-            System.out.println("Guess a Number Between 0 to 200 :  ");
+        System.out.println("Guess The Number Between 0 to 200 : ");
+        int attempt = 0;
+        int max_attempt = 10;
+
+        while (attempt < max_attempt) {
             int guess = sc.nextInt();
 
-            int attempt = 0;
-            int max_attempt = 7; // 7 Attempts Allowed
+            attempt++;
 
-            while (guess != random) {
-                attempt++; // Counts the Attempts
-                max_attempt--;
-                if (guess > 200) {
-                    System.out.println("Error...!! CHOOSE A NUMBER BETWEEN 0 TO 200");
-                    return;
-                } else if (max_attempt == 0) {
-                    System.out.println("You lose Your All Attempts Try Again...");
-                }
-
-                if (random == guess) {
-                    System.out.println("Congrats..!! Your Guess Is Correct..");
-                    return;
-                } else if (random < guess) {
-                    System.out.println("Your Guess is MORE Than the Actual Number, Choose a SMALLER Number..!! ");
-                    guess = sc.nextInt();
-                } else {
-                    System.out.println("Your Guess is LESS Than the Actual Number, Choose a GREATER  Number..!! ");
-                    guess = sc.nextInt();
-                }
-            }
-            System.out.println("Congrats..!! You Guessed The Number in " + attempt + " Attempts");
-            System.out.println("Yes OfCourse The Correct Number Was : " + random);
-
-            // Ask to Play Again
-            System.out.println("Do You Want to Play Again? (yes/no): ");
-            String choice = sc.next().toLowerCase();
-
-            if (choice.equalsIgnoreCase("no")) {
-                System.out.println("Nice to Play with you. Get Back Soon.. ");
+            if (guess == random) {
+                System.out.println("You Guessed it Right the Correct Number is: " + random);
+                System.out.println("You Guessed The Correct Answers Only in : " + attempt + " Attempts ");
                 break;
             }
+            if (guess > random) {
+                System.out.println("Too High. Try SAMALLER Number");
+            } else {
+                System.out.println("Too Low .. Try GREATER Number");
+            }
         }
-
+        if (attempt == max_attempt) {
+            System.out.println("Out of Attempts Correct Answer Was : " + attempt);
+        }
     }
 }
