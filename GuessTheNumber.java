88b6e2f1af2d455838cb5df99983da7d561d773c
niyamat <niyamatbarote@gmail.com>
2025-09-24 16:44:09 +0530

    Added the Attempt Limits

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -10,21 +10,32 @@ public class GuessTheNumber {
         System.out.println("Guess a Number Between 0 to 200 :  ");
         int guess = sc.nextInt();
 
-        if (guess > 200) {
-            System.out.println("Error...!! CHOOSE A NUMBER BETWEEN 0 TO 200");
-            return;
-        }
+        int attempt = 5;
+        int max_attempt = 5;
 
-        if (random == guess) {
-            System.out.println("Congrats..!! Your Guess Is Correct..");
-            return;
-        } else if (random < guess) {
-            System.out.println("Your Guess is MORE Than the Actual Number, Choose a SMALLER Number..!! ");
-            guess = sc.nextInt();
-        } else {
-            System.out.println("Your Guess is LESS Than the Actual Number, Choose a GREATER  Number..!! ");
-            guess = sc.nextInt();
+        while (guess != random) {
+            attempt++;
+            max_attempt--;
+            if (guess > 200) {
+                System.out.println("Error...!! CHOOSE A NUMBER BETWEEN 0 TO 200");
+                return;
+            } else if (max_attempt == 0) {
+                System.out.println("You lose Your All Attempts Try Again...");
+                return;
+            }
+
+            if (random == guess) {
+                System.out.println("Congrats..!! Your Guess Is Correct..");
+                return;
+            } else if (random < guess) {
+                System.out.println("Your Guess is MORE Than the Actual Number, Choose a SMALLER Number..!! ");
+                guess = sc.nextInt();
+            } else {
+                System.out.println("Your Guess is LESS Than the Actual Number, Choose a GREATER  Number..!! ");
+                guess = sc.nextInt();
+            }
         }
+        System.out.println("Congrats..!! You Guessed The Number in " + attempt + " Attempts");
 
         System.out.println("The Correct Number Was : " + random);
 
