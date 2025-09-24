fc8083b687c302d80957d1b7e2975a4e30e743e2
niyamat <niyamatbarote@gmail.com>
2025-09-24 14:13:43 +0530

    Guess Number Game Created

---
+++ GuessTheNumber.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rd = new Random();
+
+        int random = rd.nextInt(201);
+        System.out.println("Guess a Number Between 0 to 200 :  ");
+        int guess = sc.nextInt();
+
+        if (guess > 200) {
+            System.out.println("Error...!! CHOOSE A NUMBER BETWEEN 0 TO 200");
+            return;
+        }
+
+        if (random == guess) {
+            System.out.println("Congrats..!! Your Guess Is Correct..");
+            return;
+        } else if (random < guess) {
+            System.out.println("Your Guess is MORE Than the Actual Number, Choose a SMALLER Number..!! ");
+            guess = sc.nextInt();
+        } else {
+            System.out.println("Your Guess is LESS Than the Actual Number, Choose a GREATER  Number..!! ");
+            guess = sc.nextInt();
+        }
+
+        System.out.println("The Correct Number Was : " + random);
+
+    }
+}
